789aac1157cb2de456d77b0b2dbb8da316dcf333
WarunaUdara <warunaudarasam2003@gmail.com>
2026-07-12 20:02:23 +0530

    feat(api-gateway): support injecting http request into context

--- services/api-gateway/internal/middleware/responsewriter.go
+++ services/api-gateway/internal/middleware/responsewriter.go
@@ -6,10 +6,12 @@ import (
 )
 
 type responseWriterKey struct{}
+type requestKey struct{}
 
 func WithResponseWriter(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx := context.WithValue(r.Context(), responseWriterKey{}, w)
+		ctx = context.WithValue(ctx, requestKey{}, r)
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
@@ -17,4 +19,9 @@ func WithResponseWriter(next http.Handler) http.Handler {
 func ResponseWriterFromContext(ctx context.Context) (http.ResponseWriter, bool) {
 	w, ok := ctx.Value(responseWriterKey{}).(http.ResponseWriter)
 	return w, ok
+}
+
+func RequestFromContext(ctx context.Context) (*http.Request, bool) {
+	r, ok := ctx.Value(requestKey{}).(*http.Request)
+	return r, ok
 }
